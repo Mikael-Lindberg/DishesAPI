ab3ecaf2b505aef9886a45c679c4ec7876d1104a
mikael <mikael.lindberg93@gmail.com>
2024-03-21 09:18:42 +0100

    added endpoint filter to not allow updating rendang

--- Extensions/EndpointRouteBuilderExtensions.cs
+++ Extensions/EndpointRouteBuilderExtensions.cs
@@ -13,7 +13,25 @@ public static class EndpointRouteBuilderExtensions
         dishWithGuidIdEndpoints.MapGet("", DishesHandlers.GetDishByIdAsync).WithName("GetDish");
         dishesEndpoints.MapGet("/{dishName}", DishesHandlers.GetDishByNameAsync);
         dishesEndpoints.MapPost("", DishesHandlers.CreateDishAsync);
-        dishWithGuidIdEndpoints.MapPut("", DishesHandlers.UpdateDishAsync);
+        dishWithGuidIdEndpoints.MapPut("", DishesHandlers.UpdateDishAsync)
+            .AddEndpointFilter(async (context, next) =>
+            {
+                var dishId = context.GetArgument<Guid>(2);
+                var rendangId = new Guid("fd630a57-2352-4731-b25c-db9cc7601b16");
+
+                if (dishId == rendangId)
+                {
+                    return TypedResults.Problem(new()
+                    {
+                        Status = 400,
+                        Title = "Dish is perfect and cannot be changed.",
+                        Detail = "You cannot update perfection."
+                    });
+                }
+
+                var result = await next.Invoke(context);
+                return result;
+            });
         dishWithGuidIdEndpoints.MapDelete("", DishesHandlers.DeleteDishAsync);
     }
 
